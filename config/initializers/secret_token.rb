4b3d101fe23adacb0e0ebab4a847a6f51033a4fc
David <davidjlobo@gmail.com>
2013-03-24 16:13:47 -0400

    me trying to fix this push problem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Omrails::Application.config.secret_token = 'b1dd9b8c0427065deb6d855621b6f883ec14405494969328787049f0bd9a9676a3171267f4ce7ca025b20f6f44f8615e17225a07b835b6ff67803d6360f2c373'
